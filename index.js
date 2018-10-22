1c03281639cad0285178c363e493088b81daaa37
Crystal B <crystalbohacek@gmail.com>
2018-10-22 16:51:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function currentLine(katzDeliLine){
 
     for (var i = 0; i < katzDeliLine.length; i++){
       if (i === katzDeliLine.length-1){
-        string += `${i+1}. ${katzDeliLine[i]} `
+        string += `${i+1}. ${katzDeliLine[i]}`
       } else {
         string += `${i+1}. ${katzDeliLine[i]}, `
       }
