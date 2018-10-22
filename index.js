eec1ee2db8f85a446aee8e8f3d1df425acf0a8ee
Crystal B <crystalbohacek@gmail.com>
2018-10-22 16:37:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-function takeANumber(currentLine, name){
-    let number = currentLine.length + 1;
+function takeANumber(line, name){
+    let number = line.length + 1;
     return `Welcome ${name}. You are number ${number} in line.`
+}
+
+function nowServing(katzDeliLine){
+    katzDeliLine.splice(0, 1);
+}
+
+function currentLine(katzDeliLine){
+    var string = "The line is currently: "
+    for (var i = 0; i < katzDeliLine.length-1; i++){
+       string += `${i+1}. ${katzDeliLine[i]}, `
+    }
+    return string;
 }
